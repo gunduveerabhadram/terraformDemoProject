65871fb643cfb530d6af9b68071b4d60ba3e28f8
gunduveerabhadram <145108522+gunduveerabhadram@users.noreply.github.com>
2026-04-06 15:40:41 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,16 +1,13 @@
-
 terraform {
-  required_version = ">=1.4.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "4.17.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = "8d4e463b-c3e4-4604-9d08-45ec5ab33ec8"
-
 }
